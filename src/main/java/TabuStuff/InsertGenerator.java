ac29d19c8e241a3d7200dc56a96bcf781807a415
Zawoj <zawojweb@gmail.com>
2022-05-03 13:06:47 +0200

    Insert implementation

--- src/main/java/TabuStuff/InsertGenerator.java
+++ src/main/java/TabuStuff/InsertGenerator.java
@@ -17,16 +17,35 @@ public class InsertGenerator implements AreaGenerator {
             for (int m = 0; m < costMatrix.length; m++) {
                 if (bannedMatrix[n][m])
                     continue;
-                int mm = (m + 1) % costMatrix.length;
-                if (n == mm)
-                    continue;
-                int nn = ((n - 1) + costMatrix.length) % costMatrix.length;
-                // int newCost = currentCost - (costMatrix[result.get(nn)][result.get(n)] +
-                // costMatrix[result.get(m)][result.get(mm)] +
-                // costMatrix[result.get(index)][nn]);
+
+                int nPrev = n - 1;
+                int mPrev = m - 1;
+                int mNext = m + 1;
+                int newCost;
+
+                int beforeSwapCost = currentCost - (costMatrix[result.get(nPrev)][result.get(n)]
+                        + costMatrix[result.get(mPrev)][result.get(m)] + costMatrix[result.get(m)][result.get(mNext)]);
+
+                newCost = beforeSwapCost + (costMatrix[result.get(nPrev)][result.get(m)]
+                        + costMatrix[result.get(m)][result.get(n)] + costMatrix[result.get(mPrev)][result.get(mNext)]);
+
+                boolean skip = bestCost > newCost;
+                if (skip) {
+                    bestCost = newCost;
+                    bestSwap[0] = n;
+                    bestSwap[1] = m;
+                }
 
             }
         }
+        bannedMatrix[tabuList[tabuIndex][0]][tabuList[tabuIndex][1]] = false;
+        bannedMatrix[tabuList[tabuIndex][1]][tabuList[tabuIndex][0]] = false;
+        result.remove(bestSwap[1]);
+        result.add(bestSwap[0] - 1, bestSwap[1]);
+        bannedMatrix[bestSwap[0]][bestSwap[1]] = true;
+        bannedMatrix[bestSwap[1]][bestSwap[0]] = true;
+        tabuList[tabuIndex][0] = bestSwap[0];
+        tabuList[tabuIndex][1] = bestSwap[1];
 
         return result;
     }
